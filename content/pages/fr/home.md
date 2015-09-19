9e5301fa509019823617f82c37fcae40c70d5e03
Sylvain Fankhauser <sylvain.fankhauser@gmail.com>
2015-09-19 18:43:38 +0200

    Initial import

---
+++ content/pages/fr/home.md
@@ -0,0 +1,12 @@
+Title: Respect des animaux, vraiment ?
+Lang: fr
+Position: 00
+Name: home
+Save_As: index.html
+URL:
+
+Inséminés, élevés et tués, les morceaux d'animaux présents dans les rayons des magasins Migros ne semblent pas être un exemple pertinent de respect ! Le sommet de la négation des intérêts des animaux est atteint avec la vente du foie gras. La Migros au-delà des belles paroles illustrant ces campagnes publicitaires fait fi de toute considération éthique pour les animaux.
+
+Le gavage interdit en Suisse depuis 1978 n'est rien d'autre qu'une torture, perpétuée sous le couvert de la sauvegarde d'une tradition. Le foie gras se trouve toujours dans les rayons des succursales romandes de Migros, ainsi que sur le site de vente en ligne LeShop. Qui plus est, cette année, l'Ecole Club Migros propose des cours de cuisine du foie gras. La Migros devient ainsi active dans la promotion de ce produit issu de la maltraitance d'animaux.
+
+La campagne demande au groupe Migros de renoncer à la vente du foie gras dans l'ensemble de ses magasins, ainsi que sur le site de vente en ligne LeShop.
---
+++ pelicanconf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Association Migros sans foie gras'
+SITENAME = u'Migras'
+SITEURL = ''
+PAGE_URL = '{slug}/'
+PAGE_LANG_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+PAGE_LANG_SAVE_AS = '{slug}/index.html'
+PAGE_ORDER_BY = 'position'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Zurich'
+
+DEFAULT_LANG = u'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['i18n_subsites']
+
+LANGUAGES = (
+    ('de', 'Deutsch'),
+    ('fr', 'Français'),
+    ('it', 'Italiano'),
+)
+
+I18N_SUBSITES = {
+    'fr': {
+        'SITENAME': 'Migras - Pour une Migros sans foie gras',
+    },
+    'it': {
+        'SITENAME': 'Migras - Per une Migros senza foie gras',
+    },
+    'de': {
+        'SITENAME': 'Migras - Für eine Migros ohne foie gras',
+    },
+}
+I18N_UNTRANSLATED_PAGES = 'remove'
